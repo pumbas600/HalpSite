ce2d816ebc60aa99c55d316fb494b19d804c2ee5
Jacob Tan <jacob.tan229@gmail.com>
2021-11-17 19:42:05 +1300

    Update TodoItem.jsx
    Added:
      - Checkbox for each list item

--- frontend/src/components/Pages/Todo/TodoItem.jsx
+++ frontend/src/components/Pages/Todo/TodoItem.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { renderItems } from './TodoList';
+import React, { useState } from "react";
+import { renderItems } from "./TodoList";
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import "./todoitem.css";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 function TodoItem({ itemData }) {
-    return (
-        <li>
-            <p>{itemData.content}</p>
-            {itemData.items && itemData.items.length !== 0 &&
-                (<ul>
-                    {renderItems(itemData.items)}
-                </ul>)
-            }
-        </li>
-    );
+  const [checked, setChecked] = useState(false);
+
+  let toggle = () => {
+    setChecked(!checked);
+  };
+  return (
+    <li>
+      <FormGroup>
+        <FormControlLabel
+          className={checked ? "checkedStyle" : "uncheckedStyle"}
+          control={
+            <Checkbox
+              defaultChecked
+              onChange={toggle}
+              sx={{
+                color: "#fff",
+                "&.Mui-checked": { color: "#FBEBA5" },
+              }}
+            />
+          }
+          label={itemData.content}
+        />
+      </FormGroup>
+      {itemData.items && itemData.items.length !== 0 && (
+        <ul>{renderItems(itemData.items)}</ul>
+      )}
+    </li>
+  );
 }
 
 export default TodoItem;
