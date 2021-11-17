2947d65e9b35f101ec9a84886092e00797d301f1
Jacob Tan <jacob.tan229@gmail.com>
2021-11-17 20:48:51 +1300

    Update Todo.jsx
    Edited indentations

--- frontend/src/components/Pages/Todo/TodoList.jsx
+++ frontend/src/components/Pages/Todo/TodoList.jsx
@@ -1,28 +1,25 @@
-import './todo.css';
+import "./todo.css";
 import React from "react";
-import TodoItem from './TodoItem';
+import TodoItem from "./TodoItem";
 
-export function renderItems(items, ifEmpty=undefined) {
-    var index = 0;
-    if (items.length !== 0) {
-        return items.map(todoItem => <TodoItem key={index++} itemData={todoItem}/>); 
-    }
-    return ifEmpty && (<li>{ifEmpty}</li>);
+export function renderItems(items, ifEmpty = undefined) {
+  var index = 0;
+  if (items.length !== 0) {
+    return items.map((todoItem) => (
+      <TodoItem key={index++} itemData={todoItem} />
+    ));
+  }
+  return ifEmpty && <li>{ifEmpty}</li>;
 }
 
 function TodoList({ listData }) {
-
-    return (
-        <div className="todo-list">
-            <h3>Todo List</h3>
-            <hr/>
-            <ul>
-                {renderItems(listData.items, 'All done!')}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="todo-list" id={listData._id}>
+      <h3>Todo List</h3>
+      <hr />
+      <ul>{renderItems(listData.items, "All done!")}</ul>
+    </div>
+  );
 }
 
 export default TodoList;
-
-
