8b211224ceeded601d9ca6c2f1414f616c6a0fc4
Pumbas600 <joshjeffers600@gmail.com>
2021-11-23 12:30:26 +1300

    Miguel's calendar shows :eyes:

--- frontend/src/components/Pages/calendar/Calendar.jsx
+++ frontend/src/components/Pages/calendar/Calendar.jsx
@@ -8,7 +8,7 @@ function Calendar() {
             <Todo />
             <Month />
             <iframe title="calendar"
-                src="https://calendar.google.com/calendar/u/0/embed?src=2l4sp244sqvtkosqnbefd331d09aetbp@import.calendar.google.com&ctz=Asia/Manila">
+                src="https://calendar.google.com/calendar/embed?src=2l4sp244sqvtkosqnbefd331d09aetbp%40import.calendar.google.com&ctz=Asia%2FManila">
             </iframe>
         </div>
     )
